5d2eec7a934d51a2293d01a2d95fb0ada4afd6f1
sergiolealandres <sergiolealandres@gmail.com>
2023-12-10 16:39:08 +0100

    s

--- parallel.cuh
+++ parallel.cuh
@@ -169,7 +169,7 @@ __global__ void generar_GPU(TABLERO *t, MOVE *jugada1,int *count1,MOVE *jugada2,
 		  jugada1_local = Generador_Movimientos(mi_tablero,count1);
       for (i=1; i < *count1; i++){
 
-        jugada1[i] = *(jugada1_local[i]);
+        jugada1[i-1] = *(jugada1_local[i]);
       }
 			*count1 = *count1-1;
 			counts[0] = 0;
